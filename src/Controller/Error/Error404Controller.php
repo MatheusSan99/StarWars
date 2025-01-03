2ab735619449e6348eef9b89409700d2a8e0f367
MatheusSan <mdsmatheus100@gmail.com>
2025-01-03 10:32:03 -0300

    fix: correcao psr4

--- src/Controller/Error/Error404Controller.php
+++ src/Controller/Error/Error404Controller.php
@@ -8,11 +8,18 @@ use Nyholm\Psr7\Response;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use StarWars\Helper\HtmlRendererTrait;
 
 class Error404Controller implements RequestHandlerInterface
 {
+    use HtmlRendererTrait;
+    
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        return new Response(404);
+        $html = $this->renderTemplate('Erro/not-found', [
+            'titulo' => 'Perdido?'
+        ]);
+
+        return new Response(200, [], $html);
     }
 }
