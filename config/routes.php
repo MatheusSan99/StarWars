895cc107ad7d65b0ce40c604f0d247e6daac693e
Matheus Oliveira <99matheussan@gmail.com>
2025-01-07 16:58:00 -0300

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -56,7 +56,7 @@ return function (App $app) {
     $app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/gtf', function () use ($app) {
         ob_start();
         require_once __DIR__ . '/../src/View/gtf.php';
-        return new Response(403, [], ob_get_clean());
+        return new Response(200, [], ob_get_clean());
     });
 
     $app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function () use ($app) {
